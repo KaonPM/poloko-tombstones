f6d662f29fe3647d942bde208a97c1a56a82fba3
Kaone <ksetae@gmail.com>
2026-06-10 16:43:13 +0200

    Fix quotes page module export

---
+++ app/admin/quotes/page.tsx
@@ -0,0 +1,14 @@
+export default function QuotesPage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        padding: "40px",
+        background: "#F4EFE6",
+      }}
+    >
+      <h1>Quotes</h1>
+      <p>Quote generation module coming soon.</p>
+    </main>
+  );
+}
